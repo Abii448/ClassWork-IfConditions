49cb1e477634b680142ab29e91e751180fc72220
Abubaker Ijaz <ijazabubaker314@gmail.com>
2020-07-12 03:54:49 -0700

    Add project files.

---
+++ chk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("A is greater");
+            }
+            else if(b>a){
+                Console.WriteLine("b is greater");
+            }
+            else
+            {
+                Console.WriteLine("Both r equl");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
